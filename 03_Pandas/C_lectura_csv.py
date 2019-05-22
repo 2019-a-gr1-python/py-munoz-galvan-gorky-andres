5a5926729c59af3f31d640763f1251d9d4790e68
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-05-22 08:59:37 -0500

    leer json

--- 03_Pandas/C_lectura_csv.py
+++ 03_Pandas/C_lectura_csv.py
@@ -6,3 +6,35 @@ Created on Wed May 22 07:53:52 2019
 @author: usrdel
 """
 
+import pandas as pd
+import os
+
+
+
+path = '/Users/usrdel/Documents/GitHub/py-munoz-galvan-gorky-andres/03_Pandas/data/csv/artwork_data.csv'
+
+
+df = pd.read_csv(
+        path,
+        nrows=100,
+        usecols=['id','artist'],
+        index_col='id'
+        )
+
+columnas_a_usar = ['id','artist','title',
+                   'medium','year','acquisitionYear',
+                   'height','width','units']
+
+df_completo = pd.read_csv(
+        path,
+        usecols=columnas_a_usar,
+        index_col='id'
+        )
+
+path_guardado = '/Users/usrdel/Documents/GitHub/py-munoz-galvan-gorky-andres/03_Pandas/data/csv/artwork_data.pickle'
+
+df_completo.to_pickle(path_guardado)
+
+df_completo_pickle = pd.read_pickle(path_guardado)
+
+df_completo.shape
